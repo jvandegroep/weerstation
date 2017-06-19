77bebb00f7f5cc2e44204f230c14f8dbf0f0f994
Jacob van de Groep <jaapgroep@hotmail.com>
2017-06-19 19:24:55 +0000

    changed timestamp

--- js/script.js
+++ js/script.js
@@ -49,23 +49,22 @@ function createCustomTable(elid, level, sensortype, station, timeunit, chartID){
         console.log("loading temp dummy data..");
         
         res = JSON.stringify({"rows":[
-            {"key":["temp",2016,3,19,20,42,"station1"],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
-            {"key":["temp",2016,7,13,14,29,"station2"],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
-            {"key":["temp",2016,7,13,15,16,"station1"],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
-            {"key":["temp",2016,7,13,17,15,"station2"],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
-            {"key":["temp",2016,9,14,10,31,"station3"],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}}
-            ]});
+            {"key":["temp",2016,3,19,20,1,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
+            {"key":["temp",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
+            {"key":["temp",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
+            {"key":["temp",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
+            {"key":["temp",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}},            ]});
       }
       
      if (!res && JSON.stringify(fullURL).indexOf('humid') >= 0){
         console.log("loading humid dummy data..");
         
         res = JSON.stringify({"rows":[
-            {"key":["humid",2016,3,19,20,42,"station1"],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
-            {"key":["humid",2016,7,13,14,29,"station2"],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
-            {"key":["humid",2016,7,13,15,16,"station1"],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
-            {"key":["humid",2016,7,13,17,15,"station2"],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
-            {"key":["humid",2016,9,14,10,31,"station3"],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
+            {"key":["humid",2016,3,19,20,42,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
+            {"key":["humid",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
+            {"key":["humid",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
+            {"key":["humid",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
+            {"key":["humid",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
             ]});
       }
       
@@ -75,7 +74,8 @@ function createCustomTable(elid, level, sensortype, station, timeunit, chartID){
       var c = [];
       for (var i = 0; i < a.rows.length; i++) {
           var row = a.rows[i];
-          var timestamp = row.key[3] + "-" + row.key[2] + "-" + row.key[1] + " " + row.key[4] + ":" + row.key[5];
+          var epoch = row.key[7] + 172800000;
+          var timestamp = new Date(epoch).toLocaleString();
           actualtable = actualtable + "<tr>" + "<td>" + timestamp + "</td>" + "<td>" + row.value.max + unit + "</td>" + "<td>" + row.key[6] + "</td>" + "</tr>";
           c.push({ time: timestamp, unitName: row.value.max});
       }
@@ -144,16 +144,16 @@ function setChartOverview(chartId, station, level, timeunit) {
         
         //RESPSONSE AANPASSEN ZODAT HUMID EN TEMP IN ÉÉN RESPONSE ZITTEN, DAARNA IN DATA PUSH BEIDE VALUES OPNEMEN
         res = JSON.stringify({"rows":[
-            {"key":["temp",2016,3,19,20,42,"station1"],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
-            {"key":["temp",2016,7,13,14,29,"station2"],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
-            {"key":["temp",2016,7,13,15,16,"station1"],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
-            {"key":["temp",2016,7,13,17,15,"station2"],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
-            {"key":["temp",2016,9,14,10,31,"station3"],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}},
-            {"key":["humid",2016,3,19,20,42,"station1"],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
-            {"key":["humid",2016,7,13,14,29,"station2"],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
-            {"key":["humid",2016,7,13,15,16,"station1"],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
-            {"key":["humid",2016,7,13,17,15,"station2"],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
-            {"key":["humid",2016,9,14,10,31,"station3"],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
+            {"key":["temp",2016,3,19,20,42,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
+            {"key":["temp",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
+            {"key":["temp",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
+            {"key":["temp",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
+            {"key":["temp",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}},
+            {"key":["humid",2016,3,19,20,42,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
+            {"key":["humid",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
+            {"key":["humid",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
+            {"key":["humid",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
+            {"key":["humid",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
             ]});
       }
       
@@ -162,7 +162,8 @@ function setChartOverview(chartId, station, level, timeunit) {
       for (var i = 0; i < a.rows.length; i++) {
           var row = a.rows[i];
           var unitName = row.key[0];
-          var timestamp = row.key[3] + "-" + row.key[2] + "-" + row.key[1] + " " + row.key[4] + ":" + row.key[5];
+          var epoch = row.key[7] + 172800000;
+          var timestamp = new Date(epoch).toLocaleString();
           data.push({ time: timestamp, temp: row.value.max, humid: 75});
           console.log("time: ", timestamp, " unitName: ", unitName, " value: ", row.value.max );
       }
