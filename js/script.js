c88dabcd140c696cf04f71589129c0609a18b9db
Jacob van de Groep <jaapgroep@hotmail.com>
2017-06-21 07:56:37 +0000

    changes script.js

--- js/script.js
+++ js/script.js
@@ -22,6 +22,10 @@ function getData(url,res){
     console.error("Timeout, cannot contact ", DBURL);
     res("");
   };
+  xhttp.onerror = function () {
+    console.log("** An error occurred during the transaction");
+    res("");
+};
   xhttp.send();
 }
 
@@ -29,12 +33,13 @@ function getData(url,res){
 function createCustomTable(elid, level, sensortype, station, timeunit, chartID){
   var d=new Date();
   var startparams;
-  if (arguments[4] == "hour") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate(),d.getUTCHours()-1, d.getUTCMinutes(), station];}
-  if (arguments[4] == "day") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate()-1,d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "week") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate()-7,d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "month") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth()-1,d.getUTCDate(),d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "year") {startparams=[sensortype, d.getUTCFullYear()-1,d.getUTCMonth(),d.getUTCDate(),d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "all") {startparams=[sensortype, d.getUTCFullYear()-10,d.getUTCMonth(),d.getUTCDate(),d.getUTCHours(), d.getUTCMinutes(), station];}
+  if (arguments[4] == "10min") {startparams=[sensortype, station, "timestamphere", d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes()-10, station];}
+  if (arguments[4] == "hour") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+1, d.getUTCMinutes(), station];}
+  if (arguments[4] == "day") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate()-1,d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[4] == "week") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[4] == "month") {startparams=[sensortype, d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[4] == "year") {startparams=[sensortype, d.getUTCFullYear()-1,d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[4] == "all") {startparams=[sensortype, d.getUTCFullYear()-10,d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
 
 	var endparams=[sensortype,{}];
   var unit = "";
@@ -49,11 +54,11 @@ function createCustomTable(elid, level, sensortype, station, timeunit, chartID){
         console.log("loading temp dummy data..");
         
         res = JSON.stringify({"rows":[
-            {"key":["temp",2016,3,19,20,1,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
-            {"key":["temp",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
-            {"key":["temp",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
-            {"key":["temp",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
-            {"key":["temp",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}},            
+            {"key":["temp","station1",1458420160000,2016,3,19,20,1],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
+            {"key":["temp","station2",1468420180000,2016,7,13,14,29],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
+            {"key":["temp","station1",1468423000000,2016,7,13,15,16],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
+            {"key":["temp","station2",1468430140000,2016,7,13,17,15],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
+            {"key":["temp","station3",1473849100000,2016,9,14,10,31],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}},            
             ]});
       }
       
@@ -61,11 +66,11 @@ function createCustomTable(elid, level, sensortype, station, timeunit, chartID){
         console.log("loading humid dummy data..");
         
         res = JSON.stringify({"rows":[
-            {"key":["humid",2016,3,19,20,42,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
-            {"key":["humid",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
-            {"key":["humid",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
-            {"key":["humid",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
-            {"key":["humid",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
+            {"key":["humid","station1",1458420160000,2016,3,19,20,42],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
+            {"key":["humid","station2",1468420180000,2016,7,13,14,29],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
+            {"key":["humid","station1",1468423000000,2016,7,13,15,16],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
+            {"key":["humid","station2",1468430140000,2016,7,13,17,15],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
+            {"key":["humid","station3",1473849100000,2016,9,14,10,31],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
             ]});
       }
       
@@ -125,12 +130,13 @@ function setChart(id, data, unitName, unit) {
 function setChartOverview(chartId, station, level, timeunit) {
   var d=new Date();
   var startparams;
-  if (arguments[4] == "hour") {startparams=[d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate(),d.getUTCHours()-1, d.getUTCMinutes(), station];}
-  if (arguments[4] == "day") {startparams=[d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate()-1,d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "week") {startparams=[d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate()-7,d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "month") {startparams=[d.getUTCFullYear(),d.getUTCMonth()-1,d.getUTCDate(),d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "year") {startparams=[d.getUTCFullYear()-1,d.getUTCMonth(),d.getUTCDate(),d.getUTCHours(), d.getUTCMinutes(), station];}
-  if (arguments[4] == "all") {startparams=[d.getUTCFullYear()-10,d.getUTCMonth(),d.getUTCDate(),d.getUTCHours(), d.getUTCMinutes(), station];}
+  if (arguments[3] == "10min") {startparams=[d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes()-10, station];}
+  if (arguments[3] == "hour") {startparams=[d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+1, d.getUTCMinutes(), station];}
+  if (arguments[3] == "day") {startparams=[d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate()-1,d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[3] == "week") {startparams=[d.getUTCFullYear(),d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[3] == "month") {startparams=[d.getUTCFullYear(),d.getUTCMonth(),d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[3] == "year") {startparams=[d.getUTCFullYear()-1,d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
+  if (arguments[3] == "all") {startparams=[d.getUTCFullYear()-10,d.getUTCMonth()+1,d.getUTCDate(),d.getUTCHours()+2, d.getUTCMinutes(), station];}
 
 	var endparams=[{}];
 	var fullURL=DBURL+'?group_level=' + level + '&startkey='+ JSON.stringify(startparams)+'&endkey='+JSON.stringify(endparams);
@@ -145,16 +151,16 @@ function setChartOverview(chartId, station, level, timeunit) {
         
         //RESPSONSE AANPASSEN ZODAT HUMID EN TEMP IN ÉÉN RESPONSE ZITTEN, DAARNA IN DATA PUSH BEIDE VALUES OPNEMEN
         res = JSON.stringify({"rows":[
-            {"key":["temp",2016,3,19,20,42,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
-            {"key":["temp",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
-            {"key":["temp",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
-            {"key":["temp",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
-            {"key":["temp",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}},
-            {"key":["humid",2016,3,19,20,42,"station1",1458420160000],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
-            {"key":["humid",2016,7,13,14,29,"station2",1468420180000],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
-            {"key":["humid",2016,7,13,15,16,"station1",1468423000000],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
-            {"key":["humid",2016,7,13,17,15,"station2",1468430140000],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
-            {"key":["humid",2016,9,14,10,31,"station3",1473849100000],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
+            {"key":["temp","station1",1458420160000,2016,3,19,20,42],"value":{"sum":99,"count":1,"min":99,"max":20,"sumsqr":9801}},
+            {"key":["temp","station2",1468420180000,2016,7,13,14,29],"value":{"sum":27,"count":1,"min":27,"max":27,"sumsqr":729}},
+            {"key":["temp","station1",1468423000000,2016,7,13,15,16],"value":{"sum":30,"count":1,"min":30,"max":30,"sumsqr":900}},
+            {"key":["temp","station2",1468430140000,2016,7,13,17,15],"value":{"sum":25,"count":1,"min":25,"max":25,"sumsqr":625}},
+            {"key":["temp","station3",1473849100000,2016,9,14,10,31],"value":{"sum":98,"count":1,"min":98,"max":26,"sumsqr":9604}},
+            {"key":["humid","station1",1458420160000,2016,3,19,20,42],"value":{"sum":99,"count":1,"min":99,"max":60,"sumsqr":9801}},
+            {"key":["humid","station2",1468420180000,2016,7,13,14,29],"value":{"sum":68,"count":1,"min":68,"max":68,"sumsqr":4624}},
+            {"key":["humid","station1",1468423000000,2016,7,13,15,16],"value":{"sum":70,"count":1,"min":70,"max":70,"sumsqr":4900}},
+            {"key":["humid","station1",1468423000000,2016,7,13,17,15,"station2",1468430140000],"value":{"sum":65,"count":1,"min":65,"max":65,"sumsqr":4225}},
+            {"key":["humid","station3",1473849100000,2016,9,14,10,31],"value":{"sum":98,"count":1,"min":98,"max":75,"sumsqr":9604}}
             ]});
       }
       
@@ -262,14 +268,14 @@ function toggled() {
     $(".page").hide();
     $(".home").show();
     createCustomTable("currenttemp", "9", "temp", "station", "10min", "homeChartTemp");
-    createCustomTable("currenthumid", "9", "humid", "station", "day", "homeChartHumid");
+    createCustomTable("currenthumid", "9", "humid", "station", "10min", "homeChartHumid");
     
     // load home page after click
     $("#hrefHome").click(function(){
       $(".page").hide();
       $(".home").show();
       createCustomTable("currenttemp", "9", "temp", "station", "10min", "homeChartTemp");
-      createCustomTable("currenthumid", "9", "humid", "station", "day", "homeChartHumid");
+      createCustomTable("currenthumid", "9", "humid", "station", "10min", "homeChartHumid");
       toggled();
     });
     
