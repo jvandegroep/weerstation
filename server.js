e9dbd7a1d20343d3bf8bc1d1481d671d1734898f
Jacob van de Groep <jaapgroep@hotmail.com>
2017-07-07 07:41:39 +0000

    test - removed semicolon

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var express=require("express");
 var staticSite = __dirname +'/public';
 var app=express();
 
-app.use('/', express.static(staticSite));
+app.use('/', express.static(staticSite))
 
 app.listen(8080,function(){console.log('listening for request on port 8080');});
